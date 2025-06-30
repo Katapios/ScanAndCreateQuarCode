c8cef972d84d02d2915dc11bba0e45343a104fd9
Dennis Ryumin <d.ryumin@sigma-it.ru>
2025-06-30 20:58:13 +0300

    Initial Commit

---
+++ ScanAndCreateQuarCode/ScanAndCreateQuarCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanAndCreateQuarCodeApp.swift
+//  ScanAndCreateQuarCode
+//
+//  Created by Денис Рюмин on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScanAndCreateQuarCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
